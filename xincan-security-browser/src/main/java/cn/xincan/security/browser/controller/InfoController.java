c41e7bc0be70850e8c34e2099d51f7c85fc9c6ac
jiangxincan <jiangxincan@hatech.com.cn>
2019-07-11 15:14:26 +0800

    添加数据库、jpa、redis配置

--- xincan-security-browser/src/main/java/cn/xincan/security/browser/controller/InfoController.java
+++ xincan-security-browser/src/main/java/cn/xincan/security/browser/controller/InfoController.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("core")
+@RequestMapping("browser")
 public class InfoController {
 
     @Value("${spring.application.name}")
