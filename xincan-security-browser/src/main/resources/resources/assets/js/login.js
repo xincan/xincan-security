abb64c35b634fca79f3b2f1a2129ec1a71360556
jiangxincan <jiangxincan@hatech.com.cn>
2019-10-09 15:52:33 +0800

    修改浏览器端登录样式以及短信发送验证码功能

--- xincan-security-browser/src/main/resources/resources/assets/js/login.js
+++ xincan-security-browser/src/main/resources/resources/assets/js/login.js
@@ -1,38 +1,53 @@
 function cambiar_login() {
     document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login";
     document.querySelector('.cont_form_login').style.display = "block";
+    document.querySelector('.cont_form_login_mobile').style.opacity = "0";
     document.querySelector('.cont_form_sign_up').style.opacity = "0";
     setTimeout(function () {
             document.querySelector('.cont_form_login').style.opacity = "1";
-        },
-        400);
+        },400);
     setTimeout(function () {
-            document.querySelector('.cont_form_sign_up').style.display = "none";
-        },
-        200);
+        document.querySelector('.cont_form_login_mobile').style.display = "none";
+        document.querySelector('.cont_form_sign_up').style.display = "none";
+        },200);
+}
+
+function cambiar_login_mobile() {
+    document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_login_mobile";
+    document.querySelector('.cont_form_login').style.opacity = "0";
+    document.querySelector('.cont_form_login_mobile').style.display = "block";
+    document.querySelector('.cont_form_sign_up').style.opacity = "0";
+    setTimeout(function () {
+        document.querySelector('.cont_form_login_mobile').style.opacity = "1";
+        },400);
+    setTimeout(function () {
+        document.querySelector('.cont_form_login').style.display = "none";
+        document.querySelector('.cont_form_sign_up').style.display = "none";
+        },200);
 }
 
 function cambiar_sign_up(at) {
     document.querySelector('.cont_forms').className = "cont_forms cont_forms_active_sign_up";
-    document.querySelector('.cont_form_sign_up').style.display = "block";
     document.querySelector('.cont_form_login').style.opacity = "0";
+    document.querySelector('.cont_form_login_mobile').style.opacity = "0";
+    document.querySelector('.cont_form_sign_up').style.display = "block";
     setTimeout(function () {
             document.querySelector('.cont_form_sign_up').style.opacity = "1";
-        },
-        100);
+        },100);
     setTimeout(function () {
-            document.querySelector('.cont_form_login').style.display = "none";
-        },
-        400);
+        document.querySelector('.cont_form_login').style.display = "none";
+        document.querySelector('.cont_form_login_mobile').style.display = "none";
+        },400);
 }
 
 function ocultar_login_sign_up() {
     document.querySelector('.cont_forms').className = "cont_forms";
-    document.querySelector('.cont_form_sign_up').style.opacity = "0";
     document.querySelector('.cont_form_login').style.opacity = "0";
+    document.querySelector('.cont_form_login_mobile').style.opacity = "0";
+    document.querySelector('.cont_form_sign_up').style.opacity = "0";
     setTimeout(function () {
-            document.querySelector('.cont_form_sign_up').style.display = "none";
-            document.querySelector('.cont_form_login').style.display = "none";
-        },
-        500);
+        document.querySelector('.cont_form_login').style.display = "none";
+        document.querySelector('.cont_form_login_mobile').style.display = "none";
+        document.querySelector('.cont_form_sign_up').style.display = "none";
+        },500);
 }
